9455f8a16e31b94ada6d1a3ca5a8bd2fd25b67f8
xinqich <timurcallo@gmail.com>
2025-10-07 20:17:04 +0300

    feat(db): set up SQLAlchemy and Agent model

--- app.py
+++ app.py
@@ -1,3 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+# <------ Database stuff ------>
+class Agents(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    codename = db.Column(db.String(30), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(40), nullable=False)
+    access = db.Column(db.Integer, default=0)
+
+    def __repr__(self):
+        return f"<Agent id={self.id}, codename='{self.codename}', phone={self.phone}, email={self.email}, access={self.access}>"
+
+
+with app.app_context():
+    db.create_all()
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
