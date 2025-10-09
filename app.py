2151fbe1e59c80b8053112d2feb55789a6e8a1f7
xinqich <timurcallo@gmail.com>
2025-10-09 20:46:08 +0300

    config(flask): set secret key

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 
 app = Flask(__name__)
+app.secret_key = '2fceab9edfcb89c2f5be6b80a40a698afe2e5718'
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
